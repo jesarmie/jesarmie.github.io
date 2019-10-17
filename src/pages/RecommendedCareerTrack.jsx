e14927bc8be7d94c3778c1af356856fb03e5f04b
jesarmie <jesarmie@users.noreply.github.com>
2019-10-17 08:36:20 -0400

    initial

---
+++ src/pages/RecommendedCareerTrack.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Card, Typography, Alert } from 'antd';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { FormattedMessage } from 'umi-plugin-react/locale';
+
+const CodePreview = ({ children }) => (
+  <pre
+    style={{
+      background: '#f2f4f5',
+      padding: '12px 20px',
+      margin: '12px 0',
+    }}
+  >
+    <code>
+      <Typography.Text copyable>{children}</Typography.Text>
+    </code>
+  </pre>
+);
+
+export default () => (
+  <PageHeaderWrapper>
+    <Card>
+      <Alert
+        message="Recommended Career Track"
+        type="success"
+        showIcon
+        banner
+        style={{
+          margin: -12,
+          marginBottom: 24,
+        }}
+      />
+    </Card>
+  </PageHeaderWrapper>
+);
